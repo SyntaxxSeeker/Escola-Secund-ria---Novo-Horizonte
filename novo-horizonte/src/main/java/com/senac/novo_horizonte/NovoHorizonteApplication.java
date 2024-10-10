f7fce0e8ce1feef513671d6bf26c67be01167813
SyntaxxSeeker <logiccoder88@gmail.com>
2024-10-10 17:57:22 -0300

    Iniciando projeto

---
+++ novo-horizonte/src/main/java/com/senac/novo_horizonte/NovoHorizonteApplication.java
@@ -0,0 +1,13 @@
+package com.senac.novo_horizonte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoHorizonteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoHorizonteApplication.class, args);
+	}
+
+}
---
+++ novo-horizonte/src/test/java/com/senac/novo_horizonte/NovoHorizonteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.novo_horizonte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoHorizonteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
