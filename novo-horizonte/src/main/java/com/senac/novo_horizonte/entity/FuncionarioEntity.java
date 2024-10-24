7b571c294ea3b146502ce7e557ef41c992d4a677
SyntaxxSeeker <logiccoder88@gmail.com>
2024-10-24 19:49:11 -0300

    Criada a entidade Funcionario

--- novo-horizonte/src/main/java/com/senac/novo_horizonte/entity/FuncionarioEntity.java
+++ novo-horizonte/src/main/java/com/senac/novo_horizonte/entity/FuncionarioEntity.java
@@ -1,14 +1,15 @@
 package com.senac.novo_horizonte.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.br.CPF;
 
 @Data
 @Entity
-@Table(name = "alunos")
-public class AlunoEntity {
+@Table(name = "funcionarios")
+public class FuncionarioEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,11 +17,13 @@ public class AlunoEntity {
     private String nome;
     @CPF(message = "Insira um CPF válido")
     private String cpf;
-    @Column(name = "data_nascimento")
-    @NotNull(message = "Data de Nascimento não pode ser nulo")
-    private String dataNascimento;
-    @NotNull(message = "Matricula não pode ser nula")
-    private int matricula;
+    @NotNull(message = "Campo 'Cargo' não pode ser nulo")
+    private String cargo;
+    @Email(message = "Insira um E-Mail válido")
+    private String email;
     @NotNull(message = "Telefone não pode ser nulo")
     private String telefone;
+    @NotNull(message = "Campo 'Nome' não pode ser nulo")
+    @Column(name = "data_contratacao")
+    private String dataContratacao;
 }
